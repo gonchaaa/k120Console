56e1890267b2cf14f37fd47507dd650cb411b98d
gonchaaa <qonceabaasova@gmail.com>
2024-01-30 13:16:35 +0400

    Test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,171 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// string int double char bool
+// int-integer
+
+//string a = "natiq";
+
+//int b = 123;
+
+//double c = 5.99;
+
+//char element = 'a';
+
+//bool a = false;
+
+//Console.WriteLine(a);
+
+
+
+//string name = "Natiq";
+//string surname = "Alıyev";
+
+//name = "Murad"; -- yenidən dəyərini dəyişmək olar
+// string name= "Qonce" -- error verəcək çünki yenidən təyin etmişik
+
+//string names = "Qonce";
+//string surnames = "Abbasova";
+
+//Console.Write(name + " " + surname);
+//Console.Write(names + " " + surnames);
+
+//Console.WriteLine(name + " " + surname);
+//Console.WriteLine(names + " " + surnames);
+
+
+//Console.WriteLine(1 + 2);
+
+int age = 21;
+
+string student = "Natiq";
+
+string sum = student + age;
+
+
+Console.WriteLine(sum);
+
+
+
+//float a = 12;
+
+//double b = 12.23;
+
+//int c = 20;
+
+//double sum = b + c;
+
+//Console.WriteLine(sum);
+
+//Console.WriteLine(c);
+
+//Console.WriteLine(b);
+
+//decimal c = 15.7676564;
+
+
+
+// bool == boolean -> true false false=0 true=1
+
+
+//bool a = true;
+
+//string b = "true";
+
+//string sum = a + b;
+
+//Console.WriteLine(sum);
+
+//char element = 'a';
+
+//string b = "a";
+
+//string sum = b + element;
+
+//Console.WriteLine( sum );
+
+
+//int a = 5;
+//a = 7;
+//Console.WriteLine(a);
+
+
+//const int b = 8; --> dəyərini dəyişə bilmiyəcəksən
+
+//b= 9;
+
+//Console.WriteLine(b);
+
+// +,-,/,%,++,--
+
+
+//int a = 10;
+
+//a++;
+
+//a = a + 1;
+
+//int b = 5;
+
+
+//b--;
+//b = b - 1;
+
+
+//b = b + 2;
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+//int sum = a + b;
+
+//int sum = b-a; 
+
+//int sum = (b - a) * (-1);
+
+//int sum = a * b;
+
+//int sum = a / b;
+
+//int sum = b / a; // tək slash tam ədədi göstərir --> 5 : 10 = 0.5
+
+//int sum2 = b % a;// faiz isə qalığı göstərir
+
+
+//Console.WriteLine(b/a);
+//Console.WriteLine(sum);
+//Console.WriteLine(sum2);
+
+
+
+//bool a = false;
+
+//if (a! == true)
+//{
+//    Console.WriteLine("dogrudur");
+//}
+//else
+//{
+//    Console.WriteLine("dogru deyil");
+//}
+
+
+//if (12 <= 0)
+//{
+//    Console.WriteLine("sert odenir");
+//}
+//else
+//{
+//    Console.WriteLine("sert odenmir");
+//}
+
+
+//int a = 12;
+
+//if (a / 2 == 6)
+//{
+//    Console.WriteLine("dogrudur");
+//}
+//else
+//{
+//    Console.WriteLine("dogru deyil");
+//}
